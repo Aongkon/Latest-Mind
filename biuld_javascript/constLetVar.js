839bb0575528e1d78e84381d38490fbdf8bbaf7e
jmaongkon <jmaongkon733@gmail.com>
2024-08-01 12:20:06 +0600

    hello

---
+++ biuld_javascript/constLetVar.js
@@ -0,0 +1,27 @@
+const kongkon = 77757843;
+let ratul = "admin@gmail.com";
+var aongkon ="3509373";
+jamin = "kotta sala"
+let lalonSha;
+
+/*  
+you cant change 'const' value because const value is constant
+*/
+
+
+/*
+without value 'let' is always undefine
+so, if you use 'let' variable you must need to add a value for the 'let'
+*/
+
+/*
+prefer not to use var 
+because of issue in block scope or functional scope
+*/
+
+ratul= "jm@gmail.com";
+aongkon = "472922";
+
+
+console.log (ratul)
+console.table([kongkon, aongkon, ratul, jamin, lalonSha]);
---
+++ biuld_javascript/dataType.js
@@ -0,0 +1,27 @@
+"use strict" //that means treat all js code newer version
+
+// alert(3 + 3)  //for that we are using node js not browser
+
+
+console.log(3 +
+     3
+    )//code readability that should be high
+
+console.log("ratul");
+
+
+let name = "roman"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to  power 53
+//bigint
+//string=""
+//boolean = true/false
+//null = standalone value
+//undefine
+//symbol= when you took about something is unique type
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
